3d64dcb3a6634fa6bb8913afaa6fb8fc6b2160b7
Aiziya <ayziya.kazikhan@fizmat.kz>
2023-02-07 06:03:29 +0300

    lsn_4_jwt

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const config = require('config')
-const authRouter = require('./routes/auth.router')
+const authRouter = require('./routes/auth.routes')
 const app = express()
 const PORT = config.get('serverPort')
 
