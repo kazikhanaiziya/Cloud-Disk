a3632f0c768586a5d51d8b6bb967e1270d4cb4dc
Aiziya <ayziya.kazikhan@fizmat.kz>
2023-02-21 08:41:03 +0600

    l7 reg_form-2

--- server/index.js
+++ server/index.js
@@ -4,9 +4,11 @@ const config = require('config')
 const authRouter = require('./routes/auth.routes')
 const app = express()
 const PORT = config.get('serverPort')
+const corsMiddleware = require('./middleware/cors.middleware')
 
+app.use(corsMiddleware)
 app.use(express.json())
-app.use("/api/auth" , authRouter)
+app.use("/api/auth", authRouter)
 
 const start = async () => {
     try {
